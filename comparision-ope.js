c273d121c7ad52b6a6daffac9a39ba02791170c7
manaranjan-fullstack <mardanamanaranjan@gmail.com>
2026-08-23 09:13:44 +0530

    comparision operator in js

---
+++ comparision-ope.js
@@ -0,0 +1,9 @@
+let boy_age = 21;
+let girl_age = 18;
+
+console.log("Is boy older than girl?", boy_age > girl_age);
+console.log("Is girl older than boy?", girl_age > boy_age);
+console.log("Is boy age equal to girl age?", boy_age === girl_age);
+console.log("Is boy age not equal to girl age?", boy_age !== girl_age);
+console.log("Is boy age greater than or equal to girl age?", boy_age >= girl_age);
+console.log("Is girl age less than or equal to boy age?", girl_age <= boy_age);
