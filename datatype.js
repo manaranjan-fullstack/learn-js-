1bd30708138bbc41b05fdf97b73d6cf4c2184298
manaranjan-fullstack <mardanamanaranjan@gmail.com>
2026-08-23 08:31:36 +0530

    datatypes in js

---
+++ datatype.js
@@ -0,0 +1,12 @@
+
+let string = "hello coders";
+let number = 42;
+let boolean = true;
+let array = ["JavaScript", "Python", "Java"];
+let object = { name: "Manaranjan", age: 21, stream: "BCA" }; // key value pair
+
+console.log("String:", string);
+console.log("Number:", number);
+console.log("Boolean:", boolean);
+console.log("Array:", array);
+console.log("Object:", object);
